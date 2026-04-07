28ad1cc605ed6170cdcd44097ffb01a37d118f85
Ahhmed Albabli <ahbabli77@gmail.com>
2026-04-07 16:07:40 +0100

    update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,16 +5,18 @@
 
 
      <div class="max-w-2xl mx-auto">
+        @foreach($chirps as $chirp )
             <div class="card bg-base-100 shadow mt-8">
                 <div class="card-body">
                     <div>
-                        <h1 class="text-3xl font-bold">Welcome to Chirper!</h1>
-                        <padd class="mt-4 text-base-content/60">This is your brand new Laravel application. Time to make it
-                            sing (or chirp)!</p>
-                            <p> we are live on the internt</p>
+                       <div class="font-semibold">{{ $chirp['author'] }} </div>
+                       <div class="mt-1">{{ $chirp['message'] }} </div>
+                       <div class="tesxt-sm text-gray-500 mt-2">{{ $chirp['time'] }} </div>
+
                     </div>
                 </div>
             </div>
+            @endforeach
         </div>
 
 </x-layout>
