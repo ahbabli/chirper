56c478b52c22f56611c0d82f1f872cbd97dcd144
Ahhmed Albabli <ahbabli77@gmail.com>
2026-04-22 19:34:43 +0100

    update

--- routes/web.php
+++ routes/web.php
@@ -8,12 +8,12 @@ use App\Http\Controllers\ChirpController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth')->group(function () {
-    Route::get('/', [ChirpController::class, 'index']);
     Route::post('/chirps', [ChirpController::class, 'store']);
     Route::get('/chirps/{chirp}/edit', [ChirpController::class, 'edit']);
     Route::put('/chirps/{chirp}', [ChirpController::class, 'update']);
     Route::delete('/chirps/{chirp}', [ChirpController::class, 'destroy']);
 });
+    Route::get('/', [ChirpController::class, 'index']);
 
 Route::view('/register', 'auth.register')->middleware('guest')->name('register');
 
